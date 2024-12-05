35001c654d7404b8be56fbd21db5a202386545cd
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2024-12-06 01:18:18 +0900

    fix: vercel 사용으로 복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-// import {redirect} from "next/navigation";
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import {redirect} from "next/navigation";
 
 export default function App() {
-  // redirect('/home')
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/home');
-  }, [router]);
-
-  return null; // 초기 화면에서는 아무것도 렌더링하지 않음
+  redirect('/home')
 }
