154800d14972ffae2ce99dc628e6699e56c48c92
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2024-12-06 00:39:39 +0900

    feat: 메뉴 셋팅

--- src/app/_component/Menu/page.tsx
+++ src/app/_component/Menu/page.tsx
@@ -1,20 +1,40 @@
 import Image from 'next/image'
 import style from './style.module.scss'
 
-export type MenuProps = {
-  alt: string;
-  src: string;
-  name: string;
-
+type menu = {
+  src: string,
+  name: string
 }
+const Menus: menu[] = [
+  {
+    src: '/icon/icon-home.svg',
+    name: '홈',
+  },
+  {
+    src: '/icon/icon-coin.svg',
+    name: '자산관리',
+  },
+  {
+    src: '/icon/icon-cart.svg',
+    name: '상품',
+  },
+  {
+    src: '/icon/icon-gift.png',
+    name: '혜택',
+  },
+  {
+    src: '/icon/icon-line-three.svg',
+    name: '전체',
+  },
+]
 
-const Menu = ({ src, name, alt }: MenuProps) => {
-  return (
-    <div className={style.wrapper}>
-      <Image src={src} alt={alt} width={24} height={24} />
-      <div className={style.name}>{name}</div>
+const Menu = () => {
+  return Menus.map(menu => {
+    return <div className={style.wrapper} key={menu.src}><Image src={menu.src} alt={'12'} width={24} height={24} />
+      <div className={style.name}>{menu.name}</div>
     </div>
-  )
+  })
+
 }
 
 
--- src/app/_component/TabBar/page.tsx
+++ src/app/_component/TabBar/page.tsx
@@ -1,37 +1,11 @@
 import style from './style.module.scss'
-import Menu from "@/app/_component/Menu/page";
+import Menu from '@/app/_component/Menu/page'
 
-type menu = {
-    src:string,
-    name:string
-}
+
 const TabBar = () => {
-    const Menus: menu[] = [
-        {
-            src: '/icon/icon-home.svg',
-            name: '홈'
-        },
-        {
-            src: '/icon/icon-coin.svg',
-            name: '자산관리'
-        },
-        {
-            src: '/icon/icon-cart.svg',
-            name: '상품'
-        },
-        {
-            src: '/icon/icon-gift.png',
-            name: '혜택'
-        },
-        {
-            src: '/icon/icon-line-three.svg',
-            name: '전체'
-        },
-    ]
-
-    return <div className={style.wrapper}>
-        {Menus.map(menu => <Menu key={menu.src} name={menu.name} src={menu.src} alt={menu.name}/>)}
-    </div>
-};
+  return <div className={style.wrapper}>
+    <Menu />
+  </div>
+}
 
 export default TabBar
