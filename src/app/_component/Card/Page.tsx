c17c602d6596aa93d9b204b265c6bca84eb5d332
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2024-12-08 03:08:19 +0900

    fix: button href 필수값 기입

--- src/app/_component/Card/Page.tsx
+++ src/app/_component/Card/Page.tsx
@@ -9,18 +9,18 @@ const Card = () => {
         <div className={styles.accountName}>저축예금통장</div>
         <div className={styles.accountNumber}>
           123456-78-910112
-          <Image src="/icon/icon-copy.svg" alt="copy" width={17} height={18} className={styles.image}/>
+          <Image src="/icon/icon-copy.svg" alt="copy" width={17} height={18} className={styles.image} />
         </div>
       </div>
-      <div><Button name="설정" type="Outline" size="Small" status="Default" /></div>
+      <div><Button name="설정" type="Outline" size="Small" status="Default" href="" /></div>
     </header>
     <main className={styles.contents}>
       <div className={styles.amount}>12,345,600원</div>
-      <div className={styles.reload}><Image src="/icon/icon-reload.svg" alt="reload" width={20} height={20}/></div>
+      <div className={styles.reload}><Image src="/icon/icon-reload.svg" alt="reload" width={20} height={20} /></div>
     </main>
     <footer className={styles.btnGroup}>
-      <Button name="거래내역" type="Soft" status="Default" size="Medium"/>
-      <Button name="이체하기" type="Fill" status="Default" size="Medium" href="/home/transfer"/>
+      <Button name="거래내역" type="Soft" status="Default" size="Medium" href="" />
+      <Button name="이체하기" type="Fill" status="Default" size="Medium" href="/home/transfer" />
     </footer>
   </div>
 }
