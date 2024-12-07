4b3a6efb3dc33f014d819a505e4465af32067aec
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2024-12-08 03:33:13 +0900

    fix: url에 따라안보이게 설정

--- src/app/_component/MenuTabBar/page.tsx
+++ src/app/_component/MenuTabBar/page.tsx
@@ -1,8 +1,15 @@
+'use client'
+
 import style from './style.module.scss'
 import Menu from '@/app/_component/Menu/page'
-
+import { usePathname } from 'next/navigation';
 
 const TabBar = () => {
+  const pathname = usePathname();
+  // transfer 경로에서 메뉴바 숨기기
+  const hideMenuBar = pathname === '/home/transfer';
+  if(hideMenuBar) return null
+
   return <div className={style.wrapper}>
     <Menu />
   </div>
--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,25 +1,20 @@
-'use client'
-
 import React from 'react'
 import styles from './_style/layout.module.scss'
 import MenuTabBar from '@/app/_component/MenuTabBar/page'
-import { usePathname } from 'next/navigation';
+
 
 type layoutProps = {
   children: React.ReactNode
 }
 
 const layout = ({ children }: layoutProps) => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const pathname = usePathname();
-  // transfer 경로에서 메뉴바 숨기기
-  const hideMenuBar = pathname === '/home/transfer';
+
 
   return <div className={styles.container}>
     <main className={styles.body}>
       {children}
     </main>
-    {!hideMenuBar && <MenuTabBar />}
+    <MenuTabBar />
   </div>
 }
 
