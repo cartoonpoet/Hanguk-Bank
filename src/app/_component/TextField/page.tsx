6d77fdfd32a1ac6f25c9d769f405a6335c79a5d0
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2024-12-10 23:59:21 +0900

    chore: 계좌번호 Close 버튼 동작 추가

--- src/app/_component/TextField/page.tsx
+++ src/app/_component/TextField/page.tsx
@@ -1,11 +1,15 @@
+'use client'
+
 import styles from './style.module.scss'
 import Image from 'next/image'
+import { useState } from 'react'
 
 const TextField = () => {
+  const [value, setValue] = useState('')
   return <div className={styles.wrapper}>
-    <input placeholder="계좌번호 입력" className={styles.input}/>
-    <button className={styles.btn}><Image src="/icon/nav-close.svg" width={13.3} height={13.3} alt="close"/></button>
+    <input placeholder="계좌번호 입력" className={styles.input} value={value} onChange={(e) => setValue(e.target.value)} />
+    <button className={styles.btn} onClick={() => (setValue(''))}><Image src="/icon/nav-close.svg" width={13.3} height={13.3} alt="close" /></button>
   </div>
-};
+}
 
 export default TextField
