3c0ad1e5ab03b6e721b97176e00103fdcc0db839
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2024-12-11 00:58:06 +0900

    feat: 계좌 필터란 추가

--- src/app/home/transfer/page.tsx
+++ src/app/home/transfer/page.tsx
@@ -2,6 +2,7 @@ import NavBar from '@/app/_component/NavBar/page'
 import styles from './_style/page.module.scss'
 import TextField from '@/app/_component/TextField/page'
 import ContentSwitcher from '@/app/_component/ContentSwitcher/page'
+import SearchBar from '@/app/_component/SearchBar/page'
 
 const Transfer = () => {
   return <div className={styles.container}>
@@ -11,6 +12,7 @@ const Transfer = () => {
     </header>
     <TextField/>
     <ContentSwitcher/>
+    <SearchBar/>
   </div>
 
 }
