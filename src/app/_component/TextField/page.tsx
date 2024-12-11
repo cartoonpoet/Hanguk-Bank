283ba1826db1aa1ec233d2647fe7dafe9382dcac
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2024-12-12 01:04:05 +0900

    chore: TextField 컴포넌트에 값 입력시 제거하는 화면 표시

--- src/app/_component/TextField/page.tsx
+++ src/app/_component/TextField/page.tsx
@@ -8,7 +8,7 @@ const TextField = () => {
   const [value, setValue] = useState('')
   return <div className={styles.wrapper}>
     <input placeholder="계좌번호 입력" className={styles.input} value={value} onChange={(e) => setValue(e.target.value)} />
-    <button className={styles.btn} onClick={() => (setValue(''))}><Image src="/icon/nav-close.svg" width={13.3} height={13.3} alt="close" /></button>
+    {value.trim() && <button className={styles.btn} onClick={() => (setValue(''))}><Image src="/icon/nav-close.svg" width={13.3} height={13.3} alt="close" /></button>}
   </div>
 }
 
