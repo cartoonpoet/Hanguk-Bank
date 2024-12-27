3de3ef9f928f71430b3cce0782a24c51e13d6aed
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2024-12-28 00:41:21 +0900

    chore: 하단 네비바 병렬 라우팅으로 변경

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import styles from './_style/layout.module.scss'
-import MenuTabBar from '@/app/_component/MenuTabBar/page'
+// import MenuTabBar from '@/app/_component/MenuTabBar/page'
 
 
 type layoutProps = {
   children: React.ReactNode
+  menu: React.ReactNode
 }
 
-const layout = ({ children }: layoutProps) => {
-
-
+const layout = ({ children, menu, }: layoutProps) => {
   return <div className={styles.container}>
     <main className={styles.body}>
       {children}
     </main>
-    <MenuTabBar />
+    {/*<MenuTabBar />*/}
+    {menu}
   </div>
 }
 
