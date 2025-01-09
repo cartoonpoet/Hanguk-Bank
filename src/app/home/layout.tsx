ad59a8cf6fcf422345fb63487d66c956072387f8
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2025-01-10 02:23:22 +0900

    fix: 미사용 컴포넌트 제거

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,4 +1,3 @@
-import MenuTabBar from '@/app/_component/MenuTabBar/page'
 import React from 'react'
 import styles from './_style/layout.module.scss'
 
@@ -14,7 +13,6 @@ const layout = ({ children, menu, }: layoutProps) => {
     <main className={styles.body}>
       {children}
     </main>
-    {/*<MenuTabBar />*/}
     {menu}
   </div>
 
