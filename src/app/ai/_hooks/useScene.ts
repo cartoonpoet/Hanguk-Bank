a343869cf507b950defcb3fb0f2aea046cf082c4
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2025-01-10 02:16:52 +0900

    fix: Text To Speech 테스트

---
+++ src/app/ai/_hooks/useScene.ts
@@ -0,0 +1,56 @@
+import { useEffect, useState, MutableRefObject } from 'react'
+import { Scene, SceneOptions } from '@soulmachines/smwebsdk'
+import {
+  StateResponseBody,
+} from '@soulmachines/smwebsdk/lib-esm/websocket-message/scene/response-body/StateResponseBody'
+
+
+const useScene = (videoRef: MutableRefObject<null>) => {
+  const [text, setText] = useState('') // 텍스트 입력 상태
+  const [scene, setScene] = useState<Scene | null>(null) // Soul Machines Scene 객체
+
+  useEffect(() => {
+    const initScene = async () => {
+      const videoElement = videoRef.current
+
+      const options: SceneOptions = {
+        apiKey: 'eyJzb3VsSWQiOiJkZG5hLXNveWktaHdhbmctb3JnMTU0My0tc295YmFuayIsImF1dGhTZXJ2ZXIiOiJodHRwczovL2RoLnNvdWxtYWNoaW5lcy5jbG91ZC9hcGkvand0IiwiYXV0aFRva2VuIjoiYXBpa2V5X3YxX2QyZjhmMDA3LWY2YWYtNGM1Zi1iZDRkLWEwZWQ3ZDg5MWQxYSJ9', // 발급받은 API 키
+        videoElement: videoElement || undefined,
+        requestedMediaDevices: { microphone: true, camera: false },
+        requiredMediaDevices: { microphone: false, camera: false },
+      }
+
+      // Soul Machines Scene 초기화
+      const smScene = new Scene(options)
+
+      try {
+        // 연결 및 비디오 시작
+        const sessionId = await smScene.connect()
+        console.log('Session connected:', sessionId)
+        await smScene.startVideo()
+        setScene(smScene)
+
+        smScene.onStateEvent.addListener((scene: Scene, event: StateResponseBody) => {
+          const personaState = event.persona?.['1']
+
+          if (personaState?.speechState === 'speaking') {
+            const personaSpeech = personaState?.currentSpeech
+            console.log('[personaSpeech]', personaSpeech)
+          }
+        })
+
+      } catch (error) {
+        console.error('Error initializing scene:', error)
+      }
+    }
+    if (!scene) initScene()
+
+    return () => {
+      if (scene) scene.disconnect()
+    }
+  }, [scene])
+
+  return { text, setText, scene }
+}
+
+export default useScene
