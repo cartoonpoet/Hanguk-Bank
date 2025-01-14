b9a685c9d3ee5d9261095409e05b0baf1407740d
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2025-01-15 01:38:22 +0900

    fix: possibly 대응

--- src/app/ai/_component/Confirm/page.tsx
+++ src/app/ai/_component/Confirm/page.tsx
@@ -19,7 +19,7 @@ const Page = () => {
                 <div className={styles.sub}>받는 분과 금액을 한 번 더 확인해주세요.</div>
                 <button className={styles.account}
                         onClick={async () => await handleSpeak(scene, '김손자에게 100,000원 보내줘.')}>
-                    {getBankIcon(selectedTo.accountType)}
+                    {getBankIcon(selectedTo.accountType as string)}
                     <div className={styles.accountInfo}>
                         <div className={styles.name}>{selectedTo.name}</div>
                         <div className={styles.info}>{selectedTo.accountType} {selectedTo.accountNumber}</div>
