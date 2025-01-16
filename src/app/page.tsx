3668666228aa9c0aa7a4dca96535883e0ae441ae
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2025-01-17 00:37:50 +0900

    fix: 배경색 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import useCommonStore from "@/_hooks/useCommonStore";
 
 export default function App() {
   const { setMode } = useCommonStore();
-  return <div className="flex flex-col justify-center items-center h-dvh gap-10">
+  return <div className="flex flex-col justify-center items-center h-dvh gap-10 bg-white">
     <div onClick={() => setMode('simple')}><Button name="간편모드" type="Fill" status="Default" size="Large" href={URL.home}/></div>
     <div onClick={() => setMode('voice')}><Button name="음성모드" type="Soft" status="Default" size="Large" href={URL.home}/></div>
     <div onClick={() => setMode('ai')}><Button name="AI모드" type="Outline" status="Default" size="Large" href={URL.home}/></div>
