d51f703a9414f3c715bbeef13a0e51e010b2722a
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2025-01-28 00:54:38 +0900

    chore: useTimer로 코드 공통화 적용

--- src/app/ai/_component/Balance/page.tsx
+++ src/app/ai/_component/Balance/page.tsx
@@ -2,26 +2,14 @@
 
 import styles from './style.module.scss'
 import FloatingButton from '@/app/home/_components/floating-button'
-import Mic from '/public/icon/icon-mic.svg'
 import React, { useContext } from 'react'
 import { AiContext } from '@/_contexts/useAiContext'
 import { getBankIcon } from '@/_utils/Bank'
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import { URL } from '@/_constants/url'
+import { useTimer } from '@/_hooks/useTimer'
 
 const Page = () => {
-  const { scene, selectedTo, setWork } = useContext(AiContext)
-  const router = useRouter()
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setWork(null)
-      router.replace(URL.home)
-    }, 5000)
-    return () => clearTimeout(timer) // 타이머 클리어
-  }, [])
-
+  const { scene, selectedTo } = useContext(AiContext)
+  useTimer()
   if (!scene) return null
   return (
     <div className={styles.container}>
