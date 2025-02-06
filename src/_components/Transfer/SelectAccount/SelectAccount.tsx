388bd5b679cf1df2aa98b2133321e4b61b3071bd
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2025-02-07 00:44:24 +0900

    feat: useFunnel 적용

--- src/_components/Transfer/SelectAccount/SelectAccount.tsx
+++ src/_components/Transfer/SelectAccount/SelectAccount.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState } from 'react'
-import styles from './who.module.scss'
+import styles from './selectAccount.module.scss'
 import NavBar from '@/_components/common/NavBar/page'
 import TextField from '@/_components/common/TextField/page'
 import ContentSwitcher from '@/_components/common/ContentSwitcher/page'
@@ -10,7 +10,11 @@ import { accountRows } from '@/_constants/model'
 import AccountRow from '@/_components/common/AccountRow/page'
 import Button from '@/_components/common/Button/page'
 
-const Who = () => {
+interface SelectAccountProps {
+  onNext: () => void
+}
+
+const SelectAccount = ({onNext}: SelectAccountProps) => {
   const [selected, setSelected] = useState<string>('')
   return (
     <div className={styles.container}>
@@ -40,15 +44,14 @@ const Who = () => {
 
       <div className={styles.fixedBtn}>
         <Button
-          name='다음'
           type='Fill'
           status={!selected ? 'Disabled' : 'Default'}
           size='Large'
-          href='/'
-        />
+          onClick={onNext}
+        >다음</Button>
       </div>
     </div>
   )
 };
 
-export default Who;
+export default SelectAccount;
---
+++ src/_types/FunnelTypes.ts
@@ -0,0 +1,14 @@
+import { ReactElement, ReactNode } from 'react'
+
+export interface StepProps {
+  name: string;
+  children: ReactNode;
+}
+
+export interface FunnelProps {
+  children: Array<ReactElement<StepProps>>
+}
+
+export interface StepMoveProps {
+  onNext: (nextStep?: string) => void;
+}
