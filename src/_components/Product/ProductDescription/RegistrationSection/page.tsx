aa113c5f8f5c9ea709d0e7ccd4438c2a61d1175b
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2025-02-13 01:01:43 +0900

    feat: 적금 가입 화면 일부 구현

---
+++ src/_components/Product/ProductDescription/RegistrationSection/page.tsx
@@ -0,0 +1,42 @@
+import {
+  Container,
+  SubTitle,
+  Title,
+  List,
+  ListItem,
+  Ellipse,
+  ListItemSubTitle,
+  ListItemTitle,
+  ListItemText,
+  ButtonContainer, Button, IconButton,
+} from './style.css'
+import { GiftIcon, MoneyUpIcon, ShareIcon } from '@/_assets/icons'
+
+const RegistrationSection = () => {
+  return <Container>
+    <SubTitle>한 달만 유지해도 확정이자율을 드리는</SubTitle>
+    <Title>한국 스마트 적금</Title>
+    <List>
+      <ListItem>
+        <Ellipse>
+          <GiftIcon />
+        </Ellipse>
+        <ListItemSubTitle>기본 연 2.90%</ListItemSubTitle>
+        <ListItemTitle>최고 연 8.00%</ListItemTitle>
+      </ListItem>
+      <ListItem>
+        <Ellipse>
+          <MoneyUpIcon />
+        </Ellipse>
+        <ListItemSubTitle>저축한도</ListItemSubTitle>
+        <ListItemText>월 1만원 이상<br />30만원 이하</ListItemText>
+      </ListItem>
+    </List>
+    <ButtonContainer>
+      <Button>가입하기</Button>
+      <IconButton><ShareIcon width={24} height={24} fill="#28A745" /></IconButton>
+    </ButtonContainer>
+  </Container>
+}
+
+export default RegistrationSection
--- src/_components/Product/page.tsx
+++ src/_components/Product/page.tsx
@@ -1,8 +1,17 @@
-import ProductContextProvider from '@/_contexts/useProduct'
+'use client'
+
+import ProductContextProvider, { ProductContext } from '@/_contexts/useProduct'
 import FinancialProduct from '@/_components/Product/FinancialProduct/page'
+import { use } from 'react'
+import { ProductSteps } from '@/_constants/mode'
+import ProductDescription from '@/_components/Product/ProductDescription/page'
 
 const Product = () => {
-  return <FinancialProduct/>
+  const { Funnel, Step } = use(ProductContext)
+  return <Funnel>
+    <Step name={ProductSteps[0]}><FinancialProduct /></Step>
+    <Step name={ProductSteps[1]}><ProductDescription /></Step>
+  </Funnel>
 }
 
 const Page = () => {
